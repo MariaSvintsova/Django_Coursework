cfe23c7b5606ab637e90171a2efcba6baf9ac265
MariaSvintsova <m.svintsova@it-park.tech>
2024-06-13 11:23:33 +0300

    ---

--- blog/services.py
+++ blog/services.py
@@ -4,6 +4,17 @@ from django.conf import settings
 from blog.models import Post
 
 
+# def get_queryset():
+#     if settings.CACHE_ENABLED:
+#         key = 'post_list'
+#         queryset = cache.get(key)
+#         if not queryset:
+#             queryset = list(Post.objects.filter(is_published=True))
+#             cache.set(key, queryset, 60 * 60)
+#     else:
+#         queryset = list(Post.objects.filter(is_published=True))
+#     return queryset
+
 def get_queryset():
     if settings.CACHE_ENABLED:
         key = 'post_list'
@@ -11,6 +22,9 @@ def get_queryset():
         if not queryset:
             queryset = list(Post.objects.filter(is_published=True))
             cache.set(key, queryset, 60 * 60)
+        else:
+            print("Using cached queryset")
     else:
         queryset = list(Post.objects.filter(is_published=True))
+    print(f"Queryset: {queryset}")
     return queryset
